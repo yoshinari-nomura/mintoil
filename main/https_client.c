2f4002bcbca41f8fed7f5a4a0520fbeda03b8ecf
Yoshinari Nomura <nom@nomlab.org>
2021-08-09 12:03:32 +0900

    Replace header file to be compliant to idf-v4

--- main/https_client.c
+++ main/https_client.c
@@ -11,7 +11,7 @@
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
 #include "esp_wifi.h"
-#include "esp_event_loop.h"
+#include "esp_event.h"
 #include "esp_log.h"
 #include "esp_system.h"
 
